31fa0bff5d78c9ce4827e2f6bbc201af5437cd97
enoch <enoch@brobridge.com>
2024-11-20 12:14:25 +0800

    add subscriber batch

--- gravity-subscriber.js
+++ gravity-subscriber.js
@@ -1,3 +1,5 @@
+// const { setTimeout } = require('timers/promises');
+
 module.exports = function(RED) {
 
     function SubscriberNode(config) {
@@ -73,6 +75,30 @@ module.exports = function(RED) {
 
 		let uuid = require('uuid');
 		let Gravity = require('gravity-sdk');
+		let batchSize;
+		let batchArr =  [];
+		let timerId = null;
+		let timeout = config.timeout;
+		let currentMsgAck = null;
+
+		function setTimer(){
+			if (timerId){
+				clearTimeout(timerId);
+			}
+
+			timerId = setTimeout( ()=>{
+				if (batchArr.length > 0){
+					node.send({payload:batchArr,ack:currentMsgAck.ack});
+				}
+			},timeout*1000)
+		}
+
+		function resetTimer(){
+			if (timerId){
+				clearTimeout(timerId);
+			}
+			setTimer();
+		}
 
 		async function initSubscriber(client) {
 
@@ -97,33 +123,57 @@ module.exports = function(RED) {
 				node.log(util.format('Initializing subscriber (domain=%s, product=%s, delivery=%s)', client.opts.domain, product.name, subOpts.delivery));
 			}
 
+			if (config.batchMode){
+				batchSize = 1000;
+				node.log(util.format('Batch Mode is On and timeout is %d',timeout));
+			}
+
 			let sub = await product.subscribe([], subOpts);
 
 			sub.on('event', (m) => {
-
 				setStatus('receiving');
 
-				node.send({
-					payload: {
-						seq: m.seq,
-						eventName: m.data.eventName,
-						table: m.data.table,
-						primaryKeys: m.data.primaryKeys,
-						primaryKey: m.data.primaryKey,
-						time: m.time,
-						timeNano: m.timeNano,
-						record: m.data.record,
-					},
-					natsMsgId: m.seq.toString(),
-					ack: ack.bind(m),
-				});
+				let subPayload = {
+					seq: m.seq,
+					eventName: m.data.eventName,
+					table: m.data.table,
+					primaryKeys: m.data.primaryKeys,
+					primaryKey: m.data.primaryKey,
+					time: m.time,
+					timeNano: m.timeNano,
+					record: m.data.record,
+				}
 
-				// Sent acknoledgement automatically
+				currentMsgAck = {
+					ack:ack.bind(m),
+				};
+
+				if(config.batchMode){
+					let payload = {
+						subPayload:subPayload,
+						natsMsgId: m.seq.toString()
+					}
+					batchArr.push(payload);
+					if (batchArr.length >= batchSize){
+						if(timerId){
+							clearTimeout(timerId);
+							timerId = null;
+						}
+						node.send({payload:batchArr,ack:ack.bind(m)});
+						batchArr = [];
+					}else{
+						ack.bind(m)();
+					}
+					resetTimer();
+				}else{
+					node.send({payload:subPayload,natsMsgId: m.seq.toString(),ack:ack.bind(m)});
+				}
+
 				if (!config.manuallyAck)
 					ack.bind(m)();
 			});
 
-			node.log(util.format('Subscriber is ready (domain=%s, product=%s, delivery=%s)', client.opts.domain, product.name, subOpts.delivery));
+			node.log(util.format('Subscriber is ready (domain=%s, product=%s, delivery=%s, batchMode=%s)', client.opts.domain, product.name, subOpts.delivery,config.batchMode));
 
 			node.on('close', async () => {
 				node.log(util.format('Closing subscriber (domain=%s, product=%s, delivery=%s)', client.opts.domain, product.name, subOpts.delivery));
@@ -132,6 +182,9 @@ module.exports = function(RED) {
 
 			setStatus('connected');
 		}
+
+
+
 
 		if (!node.server) {
 			setStatus('disconnected');
